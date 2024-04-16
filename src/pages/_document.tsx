90580e97c41250edccd4e8dc9136f79d88e043b5
no-support <tk2ektk3ek@gmail.com>
2024-04-16 21:32:03 +0900

    chore: README 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="ko-KR">
-      <Head>
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
